079d3f2b2ec38fcfd858ab3346f2828722ae4528
Alexander Josu <u20211b506@upc.edu.pe>
2025-04-13 17:33:22 -0500

    📊 feat: agregar historial clínico y análisis simulado con recomendaciones

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
-import { registerClinic } from '../controllers/studentController.js';
-import { getAnalysisHistory } from '../controllers/studentController.js';
+import isStudent from '../middleware/isStudent.js';
+
+import {
+  registerClinic,
+  getAnalysisHistory
+} from '../controllers/studentController.js';
 
 const router = express.Router();
 
-router.post('/register-clinic', authMiddleware, registerClinic);
-router.get('/analysis-history', authMiddleware, getAnalysisHistory);
+router.post('/register-clinic', authMiddleware, isStudent, registerClinic);
+router.get('/analysis-history', authMiddleware, isStudent, getAnalysisHistory);
 
 export default router;
